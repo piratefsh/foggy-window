f566654e4ca25bdd112f3e11a3bc19680cc4ef69
Lauren Long <laurenzlong@gmail.com>
2015-11-02 16:48:04 -0500

    made window responsive

--- app/index.js
+++ app/index.js
@@ -10,7 +10,8 @@ class FoggyWindow {
 
         // draw image
         this.scenery = new Image()
-        this.scenery.src = 'https://c1.staticflickr.com/1/653/22653732832_e08adc6b01_h.jpg'
+        //this.scenery.src = 'https://c1.staticflickr.com/1/653/22653732832_e08adc6b01_h.jpg'
+        this.scenery.src = 'http://c2.staticflickr.com/6/5773/22682099011_04b9a83085_b.jpg'
         this.scenery.onload = () => {
             this.render()
         }
@@ -19,9 +20,34 @@ class FoggyWindow {
     }
 
     render(){
+
         this.canvas.width = window.innerWidth
         this.canvas.height = window.innerHeight
-        this.context.drawImage(this.scenery, 0, 0, this.canvas.width, this.canvas.height)
+        const imgAspectRatio = this.scenery.width/this.scenery.height
+        const canvasAspectRatio = this.canvas.width/this.canvas.height
+        
+        let imgRenderHeight, imgRenderWidth, imgOffsetX, imgOffsetY
+
+        if (canvasAspectRatio <= imgAspectRatio){ //canvas is skinnier than the image
+
+            imgRenderWidth = this.canvas.height * imgAspectRatio
+            imgRenderHeight = this.canvas.height
+
+            //offset the x
+            imgOffsetX = - (imgRenderWidth - this.canvas.width) / 2
+            imgOffsetY = 0
+
+        }else{ 
+
+            imgRenderWidth = this.canvas.width
+            imgRenderHeight = this.canvas.width / imgAspectRatio
+
+            //offset the y
+            imgOffsetX = 0
+            imgOffsetY = - (imgRenderHeight - this.canvas.height) /2
+        }
+        
+        this.context.drawImage(this.scenery, imgOffsetX, imgOffsetY, imgRenderWidth, imgRenderHeight)
 
     }
 }
