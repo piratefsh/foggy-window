88f74c410208789214055e5d0ef91dd87df05f4f
Lauren Long <laurenzlong@gmail.com>
2015-11-03 14:15:02 -0500

    draw smooth lines

--- app/index.js
+++ app/index.js
@@ -10,6 +10,8 @@ class FoggyWindow {
 
     this.context = this.canvas.getContext('2d');
     this.context.scale(window.devicePixelRatio / 2, window.devicePixelRatio / 2);
+    this.startedDrawing = false;
+    this.points = [];
 
     // draw image
     this.scenery = new Image();
@@ -19,20 +21,34 @@ class FoggyWindow {
     };
 
     let moveListener = (event) => this.draw(event);
+
     window.onresize = () => this.render();
+
     this.canvas.addEventListener('mousedown', (event) => {
+        //console.log('mousedown')
+        this.draw(event);
         this.canvas.addEventListener('mousemove', moveListener);
     });
     this.canvas.addEventListener('touchstart', (event) => {
+        this.draw(event);
         this.canvas.addEventListener('touchmove', moveListener);
     });
     this.canvas.addEventListener('mouseup', (event) => {
+        //console.log('mouseup')
+        this.startedDrawing = false;
         event.target.removeEventListener('mousemove', moveListener);
+        this.points = [];
+    });
+    this.canvas.addEventListener('touchend', (event) => {
+        this.startedDrawing = false;
+        event.target.removeEventListener('touchmove', moveListener);
+        this.points = [];
     });
   }
 
   render() {
 
+    this.startedDrawing = false;
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     const imgAspectRatio = this.scenery.width / this.scenery.height;
@@ -72,6 +88,8 @@ class FoggyWindow {
 
   draw(event) {
 
+    //console.log(this.points)
+    //console.log(this.startedDrawing)
     let x, y;
 
     if ('clientX' in event) {
@@ -84,10 +102,39 @@ class FoggyWindow {
         console.error('Unknown event sent to draw function');
     }
 
-    this.context.fillStyle = '#ffffff';
-    this.context.fillRect(x, y, 3, 3);
+
+    this.context.lineCap = 'round';
+    this.context.lineWidth = 35;
+
+    if (!this.startedDrawing) {
+        this.context.beginPath();
+        this.context.moveTo(x,y);
+        this.startedDrawing = true;
+        this.context.lineTo(x+1,y+1);
+
+    } else {
+        const previousPoint = this.points[this.points.length-1];
+        const x0 = previousPoint [0]; //x coordinate or previous point in path
+        const y0 = previousPoint [1];
+        const midpointX = parseInt((x + x0)/2);
+        const midpointY = parseInt((y + y0)/2);
+
+        this.context.strokeStyle = 'rgba(255,255,255,0.05)'
+        
+        this.context.quadraticCurveTo(midpointX, midpointY, x, y);
+        debugPoint(this.context, midpointX, midpointY)
+    }
+
     this.context.stroke();
+    this.points.push([x,y]);
   }
+}
+
+function debugPoint(context, x, y){
+    context.fillStyle = "#ff0022";
+    context.fillRect(x, y, 3, 3);
+    context.stroke();
+    console.log(x, y)
 }
 
 const foggy = new FoggyWindow();
