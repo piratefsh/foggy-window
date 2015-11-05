e236690beadab36906db9bb8105851acb5e1cda5
Sher Minn Chong <sherminn.chong@gmail.com>
2015-11-04 23:14:39 -0500

    Add footer and Timer

---
+++ app/Timer.js
@@ -0,0 +1,21 @@
+export default class Timer {
+    constructor(delay, callback) {
+        this.delay = delay;
+        this.callback = callback;
+        this.timeout = null;
+        this.startTime = null;
+    }
+
+    start() {
+        this.startTime = new Date()
+        this.timeout = setTimeout(()=> {
+            this.startTime = new Date();
+            this.callback();
+        }, this.delay);
+    }
+
+    reset() {
+        clearTimeout(this.timeout)
+        this.start()
+    }
+}
